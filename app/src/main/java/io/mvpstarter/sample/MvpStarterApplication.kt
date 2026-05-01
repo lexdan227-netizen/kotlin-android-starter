062ae18650f3b93e31e2e8fadf49e40d64cb4761
lexdan227-netizen <lexdan227@gmail.com>
2026-05-01 20:45:51 +0500

    Update MvpStarterApplication.kt

--- app/src/main/java/io/mvpstarter/sample/MvpStarterApplication.kt
+++ app/src/main/java/io/mvpstarter/sample/MvpStarterApplication.kt
@@ -25,13 +25,7 @@ class MvpStarterApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        val configuration = cloud.mindbox.mobile_sdk.MindboxConfiguration.Builder(
-            this,
-            "https://api.mindbox.ru",
-            "B3XN9rTM1FCVz2hAXSyDtxTca50xL9Cj"
-        ).build()
-
-        cloud.mindbox.mobile_sdk.Mindbox.init(this, configuration, emptyList())
+        cloud.mindbox.mobile_sdk.Mindbox.init(this, "B3XN9rTM1FCVz2hAXSyDtxTca50xL9Cj", "https://api.mindbox.ru")
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
